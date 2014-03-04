ae33e62ba5a6fe7e378cd82433b3f69effcefbad
michal96 <661383@gmail.com>
2014-03-04 21:43:04 +0100

    Rakieta
    leeeci

--- Lecimy w kosmos/main.cpp
+++ Lecimy w kosmos/main.cpp
@@ -4,19 +4,33 @@ using namespace sf;
 int main()
 {
     int odleglosc = 6370;
+    int x,y;
+    x = 400;
+    y = 550;
     sf::Event zdarzenie;
     sf::RenderWindow okno( sf::VideoMode( 800, 600 ), "Lecimy w kosmos" );
     sf::Clock czas;
+    sf::RectangleShape rakieta( sf::Vector2f( 25, 50 ) );
+    rakieta.setPosition( x, y );
     while( okno.isOpen() )
     {
         while(odleglosc <= 206370)
         {
-            while( okno.pollEvent( zdarzenie ) )
+            sf::Event event;
+            while( okno.pollEvent( event ) )
             {
-                if( zdarzenie.type == sf::Event::Closed )
-                     okno.close();
+                if( event.type == sf::Event::Closed )
+                 okno.close();
+
             }
-            okno.clear(sf::Color(255,225,255));
+            if(czas.getElapsedTime().asMilliseconds() >= 3)
+            {
+                rakieta.move(0,-1);
+                czas.restart();
+            }
+
+            okno.clear( sf::Color( 0, 127, 255 ) );
+            okno.draw( rakieta );
             okno.display();
         }
         while(odleglosc > 206370)
