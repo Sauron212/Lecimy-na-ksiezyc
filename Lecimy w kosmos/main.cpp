211f7064bc923196a2f6999de14180aa32f4027e
Sauron212 <g.riegel1996@gmail.com>
2014-03-06 17:35:28 +0100

    Ruchome tlo

--- Lecimy w kosmos/main.cpp
+++ Lecimy w kosmos/main.cpp
@@ -3,34 +3,44 @@
 using namespace sf;
 int main()
 {
-    int odleglosc = 6370;
-    int x,y;
-    x = 400;
-    y = 550;
+    int odleglosc{6370},x{375},y{400};
+    // odleglosc rakeity od srodka ziemi ; x,y - wspolrzedne rakiety wzgledem okna;
+    bool start=false;
+    // ustawienie startu rakiety na klikniecie spacja.
     sf::Event zdarzenie;
     sf::RenderWindow okno( sf::VideoMode( 800, 600 ), "Lecimy w kosmos" );
     sf::Clock czas;
-    sf::RectangleShape rakieta( sf::Vector2f( 25, 50 ) );
-    rakieta.setPosition( x, y );
-    while( okno.isOpen() )
+    sf::Texture tlo_tekstura;                       /*    Stworzenie tekstury  tlo_tekstura   */
+    tlo_tekstura.loadFromFile("tlo-start.png");
+    sf::Sprite tlo;                                 /*    Ustawienie Sprite z tekstura tlo_tekstura     */
+    tlo.setTexture(tlo_tekstura);
+    tlo.setPosition(0,-4297);
+    sf::RectangleShape rakieta(sf::Vector2f(50,100));
+    rakieta.setPosition(x,y);
+    rakieta.setFillColor(sf::Color(0,0,0));
+    while(okno.isOpen())
     {
         while(odleglosc <= 206370)
         {
-            sf::Event event;
-            while( okno.pollEvent( event ) )
+            while(okno.pollEvent(zdarzenie))
             {
-                if( event.type == sf::Event::Closed )
+                if( zdarzenie.type == sf::Event::Closed )
                  okno.close();
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                 start=true;
 
             }
-            if(czas.getElapsedTime().asMilliseconds() >= 3)
+            if(czas.getElapsedTime().asMilliseconds() >= 3 && start)
             {
-                rakieta.move(0,-1);
+                if(rakieta.getPosition().y>=300 )
+                    rakieta.move(0,-1);
+                else
+                    tlo.move(0,1);
                 czas.restart();
             }
-
-            okno.clear( sf::Color( 0, 127, 255 ) );
-            okno.draw( rakieta );
+            okno.clear(sf::Color(255,255,255));
+            okno.draw(tlo);
+            okno.draw(rakieta);
             okno.display();
         }
         while(odleglosc > 206370)
