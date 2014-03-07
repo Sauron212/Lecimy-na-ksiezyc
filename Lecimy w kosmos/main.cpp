48a7c9a5f6ed63bd3eb9305f871a025b56281e72
Sauron212 <g.riegel1996@gmail.com>
2014-03-07 18:52:56 +0100

    male zmiany

--- Lecimy w kosmos/main.cpp
+++ Lecimy w kosmos/main.cpp
@@ -6,27 +6,33 @@ using namespace sf;
 using namespace std;
 
 void czas_podrozy(sf::Clock czas);
+inline void czcionka(void);
 
 sf::RenderWindow okno(sf::VideoMode(800, 600), "Lecimy w kosmos");
+sf::Font font;
 
 int main()
 {
-    int odleglosc{6370},x{375},y{400};
-    // odleglosc rakeity od srodka ziemi ; x,y - wspolrzedne rakiety wzgledem okna;
-    bool start=false;
-    // ustawienie startu rakiety na klikniecie spacja.
+    int odleglosc{6370},x{375},y{400}; // odleglosc rakeity od srodka ziemi ; x,y - wspolrzedne rakiety wzgledem okna;
+
+    bool start=false;                      // ustawienie startu rakiety na klikniecie spacja.
+
     sf::Event zdarzenie;
-    sf::Clock czas,czas_pod;
-    sf::Font font;
-    font.loadFromFile("arial.ttf");
-    sf::Texture tlo_tekstura;                       /*    Stworzenie tekstury  tlo_tekstura   */
+    sf::Clock czas,czas_pod;             // czas - okres ruchu, czas_pod - czas podró¿y
+
+    font.loadFromFile("arial.ttf");                 // ustawienie czcionki
+
+    sf::Texture tlo_tekstura;                       //    Stworzenie tekstury  tlo_tekstura
     tlo_tekstura.loadFromFile("tlo-start.png");
-    sf::Sprite tlo;                                 /*    Ustawienie Sprite z tekstura tlo_tekstura     */
+
+    sf::Sprite tlo;                                 //    Ustawienie Sprite z tekstura tlo_tekstura
     tlo.setTexture(tlo_tekstura);
     tlo.setPosition(0,-7297);
-    sf::RectangleShape rakieta(sf::Vector2f(50,100));
+
+    sf::RectangleShape rakieta(sf::Vector2f(10,110)); //  Stworzenie rakiety, wymiary, pozycja, color itp skala 1:1
     rakieta.setPosition(x,y);
     rakieta.setFillColor(sf::Color(0,0,0));
+
     while(okno.isOpen())
     {
         while(odleglosc <= 106370)
@@ -37,24 +43,22 @@ int main()
                  okno.close();
                 if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
                  start=true;
-
             }
-            if(start==false)
+            if(start==false)                // Zerowanie czasu jak jest na ziemii
                 czas_pod.restart();
-            if(czas.getElapsedTime().asMilliseconds() >= 5 && start)
+            if(czas.getElapsedTime().asMilliseconds() >= 1 && start)
             {
-                if(rakieta.getPosition().y>=300 )
+                if(rakieta.getPosition().y>=300 )   // ustawienie rakiety na srodku ekranu
                     rakieta.move(0,-1);
                 else
-                    tlo.move(0,1);
-                    odleglosc+=14;
+                    tlo.move(0,1);   // poruszanie sie tla
 
+                odleglosc+=14;
                 czas.restart();
             }
-
             okno.clear(sf::Color(255,255,255));
             okno.draw(tlo);
-            if(odleglosc>17000 && odleglosc<22000)
+            if(odleglosc>17000 && odleglosc<22000)                  // Wyswietlanie komunikatu o sferach
             {
                 sf::Text stratosfera("Weszlismy w STRATOSFERE", font, 18);
                 stratosfera.setPosition(300,400);
@@ -89,18 +93,18 @@ int main()
 }
 void czas_podrozy(sf::Clock czas)
 {
-    ostringstream ss;
-    sf::Font font;
-    font.loadFromFile("arial.ttf");
+    ostringstream ss;                       // potrzebne do konwersji z inta/clocka na stringa
     sf::Time czas2 = czas.getElapsedTime();
     int a = czas2.asSeconds();
+
             int godziny = a/3600;
             int minuty = (a-godziny*3600)/60;
             int sekundy = a-godziny*3600-minuty*60;
+
     ss<< godziny << " h " << minuty << " min " << sekundy << " sekund";
-    string czas3 = ss.str();
+    string czas3 = ss.str();                    // przekazanie ss do zmiennej string
     sf::Text czas_wys(czas3, font, 20);
     czas_wys.setColor((sf::Color::Black));
     czas_wys.setPosition(600, 500);
-    okno.draw(czas_wys);
+    okno.draw(czas_wys);                        // wyswietlenie czasu
 }
